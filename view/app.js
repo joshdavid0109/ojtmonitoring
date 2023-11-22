3a30f573f2905c64dbbc7350ba4ad4f7a18b099c
Joshua Daniel David <joshdavid0109@gmail.com>
2023-11-23 02:02:13 +0800

    joke, 'di pala pwede ibang js file yung db access.
    revert plus add code to access sql file

---
+++ view/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParse = require('body-parser');
+const fileUpload = require('express-fileupload');
+const mysql = require('mysql');
+
+const app = express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ojt'
+})
+
+connection.connect((err) => {
+    if (err) 
+        throw err;
+    console.log('Connected');
+})
+
+let query = 'SELECT * from students';
+
+connection.query(query, (error, results, fields) => {
+    if (error)
+        return console.error(error.message);
+
+    console.log(results);
+})
+
+connection.end();
+
+
