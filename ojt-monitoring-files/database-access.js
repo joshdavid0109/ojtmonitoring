ad5f21e404aa8bb9eee8f914cc941920f1286317
Joshua Daniel David <joshdavid0109@gmail.com>
2023-11-27 21:34:58 +0800

    comms, not yet done

--- ojt-monitoring-files/database-access.js
+++ ojt-monitoring-files/database-access.js
@@ -4,7 +4,7 @@ const dbConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'ojt'
+    database: 'ojt_with-data'
 })
 
 dbConnection.connect((err) => {
--- view/app.js
+++ view/app.js
@@ -2,29 +2,50 @@ const express = require('express');
 const bodyParse = require('body-parser');
 const fileUpload = require('express-fileupload');
 const mysql = require('mysql');
+const pug = require('pug');
+const app = express();
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'ojt'
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+
+    res.render(
+        'indexpug',
+        { title : 'intern list', message: 'intern list'}
+    )
 })
 
-connection.connect((err) => {
-    if (err)
-        throw err;
-    console.log('Connected');
-})
+app.listen(3000);
 
-let query = 'SELECT * from students';
 
-connection.query(query, (error, results, fields) => {
-    if (error)
-        return console.error(error.message);
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'ojt_with-data'
+// })
 
-    console.log(results);
-})
 
-connection.end();
+
+// connection.connect((err) => {
+//     if (err)
+//         throw err;
+//     console.log('Connected');
+// })
+
+
+
+// let query = 'SELECT * from students';
+
+// connection.query(query, (error, results, fields) => {
+//     if (error)
+//         return console.error(error.message);
+
+//     console.log(results);
+// })
+
+// connection.end();
+
 
 
