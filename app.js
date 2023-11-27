c436ac2d01fca5cae59f2d77d0560628956b52a6
Haydee Shane Saguid <2211010@slu.edu.ph>
2023-11-27 18:54:28 +0800

    Query student tables and render index.pug
    Rendered the index.pug file, displaying student information in a table format.

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ const express = require('express');
 const { fetchStudents } = require('./database.js');
 
 const app = express();
+const port = 8080;
+
+app.set('view engine', 'pug');
+app.set('views', './ojt-monitoring-files/ojt-pending');
 
 app.get("/", async (req, res) => {
     try {
         const students = await fetchStudents();
-        res.send(students);
-        
+        res.render('index', {students})
+        // res.send(students);   
     } catch (error) {
         console.error('Error:', error.message);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send('Warning: Internal Server Error');
     }
 })
 
 app.listen(8080, () => {
-    console.log('Server is running at port 8080')
+    console.log(`Server is running at port ${port}`);
 })
 
 
