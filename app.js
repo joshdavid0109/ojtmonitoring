389c209754ee3a4bebe82a7d4047ecac76c7b526
Haydee Shane Saguid <2211010@slu.edu.ph>
2023-11-27 19:58:18 +0800

    Address rendering issues-index.pug of ojt-pending
    -styles.css is now applied when rendering index.pug
    -images and paths of the page is now working

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const { fetchStudents } = require('./database.js');
+const path = require('path')
 
 const app = express();
 const port = 8080;
 
-app.set('view engine', 'pug');
-app.set('views', './ojt-monitoring-files/ojt-pending');
+app.use(express.static(path.join(__dirname, 'ojt-monitoring-files', 'ojt-pending')));
+app.use('/ojt-images', express.static(path.join(__dirname, 'ojt-images')));
 
-app.get("/", async (req, res) => {
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'ojt-monitoring-files', 'ojt-pending'));
+
+
+
+
+// run node app.js then access http://localhost:8080/ojt-pending/
+app.get("/ojt-pending", async (req, res) => {
     try {
         const students = await fetchStudents();
         res.render('index', {students})
